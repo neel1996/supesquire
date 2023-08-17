b8f3c2d95636dc3033fed95116fab63f430b46a7
neel1996 <47709856+neel1996@users.noreply.github.com>
2023-08-17 22:20:44 +0530

    Remove explicity model configuration

--- src/app/api/openai.js
+++ src/app/api/openai.js
@@ -1,6 +1,5 @@
 import { OpenAI } from 'langchain/llms/openai';
 
 export const llm = new OpenAI({
-  openAIApiKey: process.env.OPENAI_API_KEY,
-  modelName: 'gpt-3.5-turbo'
+  openAIApiKey: process.env.OPENAI_API_KEY
 });
