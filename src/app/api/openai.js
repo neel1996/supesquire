77fe924fa9b2ed7352f4388dc905cc7213a2fe28
neel1996 <47709856+neel1996@users.noreply.github.com>
2023-08-17 15:15:38 +0530

    Choose openai model explicitly

--- src/app/api/openai.js
+++ src/app/api/openai.js
@@ -1,5 +1,6 @@
 import { OpenAI } from 'langchain/llms/openai';
 
 export const llm = new OpenAI({
-  openAIApiKey: process.env.OPENAI_API_KEY
+  openAIApiKey: process.env.OPENAI_API_KEY,
+  modelName: 'gpt-3.5-turbo'
 });
