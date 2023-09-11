9a0e4e59b6b5b7114ba2f9cd3629e3fa71ea3d1f
neel1996 <47709856+neel1996@users.noreply.github.com>
2023-09-11 23:27:07 +0530

    Combine format prompt to the main chain

--- src/useHttpClient.js
+++ src/useHttpClient.js
@@ -4,7 +4,12 @@ export const useHttpClient = () => {
   const route = useRouter();
 
   const fetch = async (url, options) => {
-    const res = await window.fetch(url, options);
+    const res = await window.fetch(url, {
+      ...options,
+      next: {
+        revalidate: 300
+      }
+    });
 
     if (res.status === 401) {
       route.replace('/login');
